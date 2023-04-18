06a1f821724d823a41a5af6eac14b830d77b6dc4
Roland Bewick <roland.bewick@gmail.com>
2023-04-18 22:38:01 +0700

    chore: validate fields in create subscription form

--- app/create/components/CreateSubscriptionForm.tsx
+++ app/create/components/CreateSubscriptionForm.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { CreateSubscriptionFormData } from "types/CreateSubscriptionFormData";
 import { Timeframe, timeframes } from "types/Timeframe";
+import { LightningAddress } from "alby-tools";
+import { Loading } from "app/components/Loading";
 
 const inputClassNameWithoutBottomMargin = "input input-bordered w-full";
 const inputBottomMargin = "mb-4";
@@ -29,8 +31,7 @@ export function CreateSubscriptionForm() {
         : {
             amount: "1",
             recipientLightningAddress:
-              process.env.NEXT_PUBLIC_DEFAULT_LIGHTNING_ADDRESS ||
-              "hello@getalby.com",
+              process.env.NEXT_PUBLIC_DEFAULT_LIGHTNING_ADDRESS,
             message: process.env.NEXT_PUBLIC_DEFAULT_MESSAGE,
             timeframeValue:
               process.env.NEXT_PUBLIC_DEFAULT_SLEEP_TIMEFRAME_VALUE || "1",
@@ -49,6 +50,8 @@ export function CreateSubscriptionForm() {
     (timeframe: Timeframe) => setValue("timeframe", timeframe),
     [setValue]
   );
+  const [validatingLightningAddress, setValidatingLightningAddress] =
+    React.useState(false);
 
   return (
     <form
@@ -57,24 +60,69 @@ export function CreateSubscriptionForm() {
       className="flex flex-col gap-2 w-full"
     >
       <label className={labelClassName}>Recipient Lightning address</label>
-      <input
-        {...register("recipientLightningAddress")}
-        placeholder="hello@getalby.com"
-        className={inputClassName}
-      />
+      <div className="relative flex flex-col justify-center">
+        <input
+          {...register("recipientLightningAddress", {
+            validate: async (address) => {
+              setValidatingLightningAddress(true);
+              let errorMessage: string | undefined;
+              try {
+                if (!address.length) {
+                  errorMessage = "please provide a lightning address";
+                }
+                const ln = new LightningAddress(address);
+                if (!ln.username) {
+                  errorMessage = "This is not a valid lightning address";
+                }
+                if (!errorMessage) {
+                  await ln.fetch();
+                }
+                if (!ln.lnurlpData) {
+                  errorMessage = "This lightning address does not exist";
+                }
+              } catch (e) {
+                errorMessage = "This is not a valid lightning address";
+              }
+              setValidatingLightningAddress(false);
+              return errorMessage;
+            },
+          })}
+          placeholder="hello@getalby.com"
+          className={inputClassName}
+        />
+        {validatingLightningAddress && (
+          <div className="absolute right-3">
+            <Loading />
+          </div>
+        )}
+      </div>
+      {errors.recipientLightningAddress && (
+        <p className="text-error">{errors.recipientLightningAddress.message}</p>
+      )}
       <label className={labelClassName}>Amount in sats</label>
       <input
-        {...register("amount")}
+        {...register("amount", {
+          validate: (value) =>
+            parseInt(value) <= 0 || isNaN(parseInt(value))
+              ? "Please enter a positive value"
+              : undefined,
+        })}
         placeholder="21"
         className={inputClassName}
       />
+      {errors.amount && <p className="text-error">{errors.amount.message}</p>}
       <label className={labelClassName}>Frequency</label>
       <div
         className={`flex justify-center gap-2 items-center ${inputBottomMargin}`}
       >
         <p className="lg:flex-shrink-0">Repeat payment every</p>
         <input
-          {...register("timeframeValue")}
+          {...register("timeframeValue", {
+            validate: (value) =>
+              parseInt(value) <= 0 || isNaN(parseInt(value))
+                ? "Please enter a positive value"
+                : undefined,
+          })}
           className={`${inputClassNameWithoutBottomMargin} w-full`}
         />
         <select
@@ -94,6 +142,9 @@ export function CreateSubscriptionForm() {
           ))}
         </select>
       </div>
+      {errors.timeframeValue && (
+        <p className="text-error">{errors.timeframeValue.message}</p>
+      )}
       <label className={labelClassName}>Message attached</label>
       <input
         {...register("message")}
