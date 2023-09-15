1f6ebf39394a255e198c09f12a7dd6901f58c7c2
Roland Bewick <roland.bewick@gmail.com>
2023-09-15 22:56:21 +0700

    fix: remove month timeframe option

--- types/Timeframe.ts
+++ types/Timeframe.ts
@@ -1,4 +1,5 @@
-export const timeframes = ["hours", "days", "weeks", "months"] as const;
+// NOTE: months is not supported by ms library!
+export const timeframes = ["hours", "days", "weeks"] as const;
 
 if (process.env.NEXT_PUBLIC_ALLOW_SHORT_TIMEFRAMES === "true") {
   (timeframes as unknown as string[]).unshift("seconds", "minutes");
