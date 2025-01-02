d3337fe39710881bae8b625a6c437a1a164eeb52
Roland Bewick <roland.bewick@gmail.com>
2025-01-02 21:38:16 +0700

    fix: add missing CORS header

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
             key: "Access-Control-Allow-Methods",
             value: "GET, POST, PUT, DELETE, OPTIONS",
           },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
         ],
       },
     ];
